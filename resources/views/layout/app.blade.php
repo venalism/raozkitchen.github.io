00783297532a384cfb57b93b2e2e7d483c4c3a16
venalism <nadithakbar@gmail.com>
2025-05-16 21:48:51 +0700

    webhook

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,7 @@
     <title>{{ config('app.name', 'Raoz') }} - @yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script type="module" src="https://cdn.jsdelivr.net/gh/domyid/tracker@main/index.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body class="font-sans bg-gray-50">
